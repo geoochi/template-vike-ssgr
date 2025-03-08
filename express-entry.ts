30cb614b738cd58703a95bc1dbc71e60894e8d5d
Geoo Chi <geoochi@geoochi.com>
2025-03-08 15:19:37 +0800

    update

--- express-entry.ts
+++ express-entry.ts
@@ -15,43 +15,37 @@ const root = __dirname
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
 // const hmrPort = process.env.HMR_PORT ? parseInt(process.env.HMR_PORT, 10) : 24678
 
-export default (await startServer()) as unknown
+const app = express()
 
-async function startServer() {
-  const app = express()
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(`${root}/dist/client`))
-  } else {
-    // Instantiate Vite's development server and integrate its middleware to our server.
-    // ⚠️ We should instantiate it *only* in development. (It isn't needed in production
-    // and would unnecessarily bloat our server in production.)
-    // const vite = await import('vite')
-    // const viteDevMiddleware = (
-    //   await vite.createServer({
-    //     root,
-    //     server: { middlewareMode: true, hmr: { port: hmrPort } },
-    //   })
-    // ).middlewares
-    // app.use(viteDevMiddleware)
-    const { devMiddleware } = await createDevMiddleware({ root })
-    app.use(devMiddleware)
-  }
-
-  app.use(createMiddleware(dbMiddleware)())
-
-  app.post('/api/todo/create', createHandler(createTodoHandler)())
-
-  /**
-   * Vike route
-   *
-   * @link {@see https://vike.dev}
-   **/
-  app.all('*', createHandler(vikeHandler)())
-
-  app.listen(port, () => {
-    console.log(`Server listening on http://localhost:${port}`)
-  })
-
-  return app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(`${root}/dist/client`))
+} else {
+  // Instantiate Vite's development server and integrate its middleware to our server.
+  // ⚠️ We should instantiate it *only* in development. (It isn't needed in production
+  // and would unnecessarily bloat our server in production.)
+  // const vite = await import('vite')
+  // const viteDevMiddleware = (
+  //   await vite.createServer({
+  //     root,
+  //     server: { middlewareMode: true, hmr: { port: hmrPort } },
+  //   })
+  // ).middlewares
+  // app.use(viteDevMiddleware)
+  const { devMiddleware } = await createDevMiddleware({ root })
+  app.use(devMiddleware)
 }
+
+app.use(createMiddleware(dbMiddleware)())
+
+app.post('/api/todo/create', createHandler(createTodoHandler)())
+
+/**
+ * Vike route
+ *
+ * @link {@see https://vike.dev}
+ **/
+app.all('*', createHandler(vikeHandler)())
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`)
+})
